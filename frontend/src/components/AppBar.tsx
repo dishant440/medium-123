39b7ca37f260a548392cf9d9d760253e38dfe555
Dishant <dishantd442442@gmail.com>
2024-06-18 23:53:04 +0530

    added logout and login token adding and removing logic

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -8,26 +8,29 @@ export default function AppBar() {
 
   const handleAuthButtonClick = () => {
     if (isAuthenticated) {
+      localStorage.removeItem("token");
+
       logout();
     } else {
-      navigate('/signin');
+      localStorage.removeItem("token");
+      navigate("/signin");
     }
   };
 
   return (
     <div className="flex flex-row justify-between mx-8 mt-4 items-center">
       <span className="text-xl font-bold">Medium</span>
-     <div className="flex flex-row items-center gap-x-2">
-     <span className="w-10 h-10">
-        <img src={medium} alt="Medium Logo" />
-      </span>
-      <button
-        onClick={handleAuthButtonClick}
-        className="ml-4 px-4 py-2 bg-blue-500 text-white rounded"
-      >
-        {isAuthenticated ? 'Logout' : 'Login'}
-      </button>
-     </div>
+      <div className="flex flex-row items-center gap-x-2">
+        <span className="w-10 h-10">
+          <img src={medium} alt="Medium Logo" />
+        </span>
+        <button
+          onClick={handleAuthButtonClick}
+          className="ml-4 px-4 py-2 bg-black text-white rounded"
+        >
+          {isAuthenticated ? "Logout" : "Login"}
+        </button>
+      </div>
     </div>
   );
 }
